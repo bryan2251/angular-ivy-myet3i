b3cede21bfa45a767f39daae18d945bec4841a02
bryan2251 <bryan.castro@ucb.edu.bo>
2022-05-19 04:02:08 -0400

    3 files modified

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -14,19 +14,26 @@ export class SkillComponent {
   };
   datos = {
     id: '',
+    precioF: 0,
   };
   art = {
     codigo: null,
     descripcion: null,
     precio: null,
+    cantidad: null,
   };
 
   articulos = [
-    { codigo: 1, descripcion: 'Chocolate', precio: 1.5 },
-    { codigo: 2, descripcion: 'cable', precio: 15.5 },
-    { codigo: 3, descripcion: 'martillos', precio: 102.3 },
-    { codigo: 4, descripcion: 'tuberías', precio: 70 },
-    { codigo: 5, descripcion: 'pintura', precio: 60.6 },
+    { codigo: 1, descripcion: 'Chocolate Crocante', precio: 50, cantidad: 2 },
+    {
+      codigo: 2,
+      descripcion: 'Chocolate con leche',
+      precio: 100,
+      cantidad: 2,
+    },
+    { codigo: 3, descripcion: 'Chocolate relleno', precio: 40, cantidad: 2 },
+    { codigo: 4, descripcion: 'Bombones', precio: 60, cantidad: 2 },
+    { codigo: 5, descripcion: 'Chocolate amargo', precio: 50, cantidad: 2 },
   ];
 
   hayRegistros() {
@@ -51,16 +58,19 @@ export class SkillComponent {
       codigo: this.art.codigo,
       descripcion: this.art.descripcion,
       precio: this.art.precio,
+      cantidad: this.art.cantidad,
     });
     this.art.codigo = null;
     this.art.descripcion = null;
     this.art.precio = null;
+    this.art.cantidad = null;
   }
 
   seleccionar(art) {
     this.art.codigo = art.codigo;
     this.art.descripcion = art.descripcion;
     this.art.precio = art.precio;
+    this.art.cantidad = art.cantidad;
   }
 
   modificar() {
@@ -68,8 +78,16 @@ export class SkillComponent {
       if (this.articulos[x].codigo == this.art.codigo) {
         this.articulos[x].descripcion = this.art.descripcion;
         this.articulos[x].precio = this.art.precio;
+        this.articulos[x].cantidad = this.art.cantidad;
         return;
       }
     alert('No existe el código de articulo ingresado');
+  }
+  calcular() {
+    this.datos.precioF = 0;
+    for (let x = 0; x < this.articulos.length; x++) {
+      this.datos.precioF +=
+        this.articulos[x].precio * this.articulos[x].cantidad;
+    }
   }
 }
